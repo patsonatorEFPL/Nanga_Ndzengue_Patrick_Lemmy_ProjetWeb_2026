616b23c2eb5fc62705be075846e541fa2cfec9dd
BassCode <patricklemmy.nangandzengue@student.efpl.be>
2026-06-20 17:38:08 +0200

    fix(front): page fallback avec message clair au lieu du works par defaut

--- front/src/app/shared/ui/page/main-fall-back-page/main-fall-back-page.ts
+++ front/src/app/shared/ui/page/main-fall-back-page/main-fall-back-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-fall-back-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main-fall-back-page.html',
   styleUrl: './main-fall-back-page.css',
 })
